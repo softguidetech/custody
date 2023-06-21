4e938f0b1c8573e7c51c4da61597e0f2ca20a7e0
softguidetech <96677990+softguidetech@users.noreply.github.com>
2023-06-21 13:33:09 +0400

    Delete custody_clear_request directory

--- custody_clear_request/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import custody_clear_request
-from . import money_to_text_en
-from . import money_to_text_ar
-from . import custody_clear
-from . import custody_team
-from . import res_config_settings
--- custody_clear_request/reports/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import custody_request_report
-from . import pettycash_report
